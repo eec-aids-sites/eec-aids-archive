71013fda7d8324f501f8fed01ae864cae1d92b78
retraigo <nett@nett.moe>
2023-05-14 12:32:17 +0530

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,27 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus'
-})
+  extends: "@nuxt-themes/docus",
+  content: {
+    highlight: {
+      // Theme used in all color schemes.
+      theme: {
+        // Default theme (same as single string)
+        default: "dracula",
+        // Theme used if `html.dark`
+        dark: "dracula",
+        // Theme used if `html.sepia`
+        sepia: "monokai",
+      },
+      preload: [
+        "c",
+        "sql",
+        "scala",
+        "r",
+        "html",
+        "vue",
+        "javascript",
+        "typescript",
+        "prolog",
+      ],
+    },
+  },
+});
